aecd63c801a13e7766eea7bdde1e100746ace6b2
Ryan Kurte <ryankurte@gmail.com>
2017-04-27 14:44:27 +1200

    Initial commit

---
+++ infill.go
@@ -0,0 +1,103 @@
+package configurer
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+)
+
+// Parses a config struct and loads
+func infillConfig(delimiter, prefix string, config interface{}) interface{} {
+
+	original := reflect.ValueOf(config)
+
+	copy := reflect.New(original.Type()).Elem()
+	translateRecursive(delimiter, prefix, copy, original)
+
+	// Remove the reflection wrapper
+	return copy.Interface()
+}
+
+// Parses a string looking for a delimiter indicating that the value should be loaded from the environment
+func mapString(delimiter, prefix, line string) string {
+	if !strings.HasPrefix(line, delimiter) {
+		return line
+	}
+
+	key := fmt.Sprintf("%s%s", prefix, strings.Replace(line, delimiter, "", -1))
+	value := os.Getenv(key)
+
+	return value
+}
+
+// https://gist.github.com/hvoecking/10772475
+func translateRecursive(delimiter, prefix string, copy, original reflect.Value) {
+	switch original.Kind() {
+	// The first cases handle nested structures and translate them recursively
+
+	// If it is a pointer we need to unwrap and call once again
+	case reflect.Ptr:
+		// To get the actual value of the original we have to call Elem()
+		// At the same time this unwraps the pointer so we don't end up in
+		// an infinite recursion
+		originalValue := original.Elem()
+		// Check if the pointer is nil
+		if !originalValue.IsValid() {
+			return
+		}
+		// Allocate a new object and set the pointer to it
+		copy.Set(reflect.New(originalValue.Type()))
+		// Unwrap the newly created pointer
+		translateRecursive(delimiter, prefix, copy.Elem(), originalValue)
+
+	// If it is an interface (which is very similar to a pointer), do basically the
+	// same as for the pointer. Though a pointer is not the same as an interface so
+	// note that we have to call Elem() after creating a new object because otherwise
+	// we would end up with an actual pointer
+	case reflect.Interface:
+		// Get rid of the wrapping interface
+		originalValue := original.Elem()
+		// Create a new object. Now new gives us a pointer, but we want the value it
+		// points to, so we have to call Elem() to unwrap it
+		copyValue := reflect.New(originalValue.Type()).Elem()
+		translateRecursive(delimiter, prefix, copyValue, originalValue)
+		copy.Set(copyValue)
+
+	// If it is a struct we translate each field
+	case reflect.Struct:
+		for i := 0; i < original.NumField(); i += 1 {
+			translateRecursive(delimiter, prefix, copy.Field(i), original.Field(i))
+		}
+
+	// If it is a slice we create a new slice and translate each element
+	case reflect.Slice:
+		copy.Set(reflect.MakeSlice(original.Type(), original.Len(), original.Cap()))
+		for i := 0; i < original.Len(); i += 1 {
+			translateRecursive(delimiter, prefix, copy.Index(i), original.Index(i))
+		}
+
+	// If it is a map we create a new map and translate each value
+	case reflect.Map:
+		copy.Set(reflect.MakeMap(original.Type()))
+		for _, key := range original.MapKeys() {
+			originalValue := original.MapIndex(key)
+			// New gives us a pointer, but again we want the value
+			copyValue := reflect.New(originalValue.Type()).Elem()
+			translateRecursive(delimiter, prefix, copyValue, originalValue)
+			copy.SetMapIndex(key, copyValue)
+		}
+
+	// Otherwise we cannot traverse anywhere so this finishes the the recursion
+
+	// If it is a string translate it (yay finally we're doing what we came for)
+	case reflect.String:
+		translatedString := mapString(delimiter, prefix, original.Interface().(string))
+		copy.SetString(translatedString)
+
+	// And everything else will simply be taken from the original
+	default:
+		copy.Set(original)
+	}
+
+}
---
+++ infill_test.go
@@ -0,0 +1,63 @@
+package configurer
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Embedded struct {
+	KeyThree string
+}
+
+type FakeConfig struct {
+	KeyOne   string
+	Map      map[string]string
+	Embedded Embedded
+}
+
+const (
+	keyOneName    = "KEY_ONE"
+	keyOneValue   = "KEY_ONE_VALUE"
+	keyTwoIndex   = "KEY_TWO_INDEX"
+	keyTwoName    = "KEY_TWO"
+	keyTwoValue   = "KEY_TWO_VALUE"
+	keyThreeName  = "KEY_THREE"
+	keyThreeValue = "KEY_THREE_VALUE"
+)
+
+func TestConfigInfill(t *testing.T) {
+
+	t.Run("Infill strings in config structure", func(t *testing.T) {
+		prefix := "TEST_"
+		delimiter := "$"
+
+		c := FakeConfig{
+			KeyOne:   fmt.Sprintf("%s%s", delimiter, keyOneName),
+			Map:      make(map[string]string),
+			Embedded: Embedded{fmt.Sprintf("%s%s", delimiter, keyThreeName)},
+		}
+		c.Map[keyTwoIndex] = fmt.Sprintf("%s%s", delimiter, keyTwoName)
+
+		log.Printf("Config: %+v", c)
+
+		os.Setenv(fmt.Sprintf("%s%s", prefix, keyOneName), keyOneValue)
+		os.Setenv(fmt.Sprintf("%s%s", prefix, keyTwoName), keyTwoValue)
+		os.Setenv(fmt.Sprintf("%s%s", prefix, keyThreeName), keyThreeValue)
+
+		c2 := infillConfig(delimiter, prefix, &c)
+
+		config2 := c2.(*FakeConfig)
+
+		assert.EqualValues(t, keyOneValue, config2.KeyOne)
+		assert.EqualValues(t, keyTwoValue, config2.Map[keyTwoIndex])
+		assert.EqualValues(t, keyThreeValue, config2.Embedded.KeyThree)
+
+		log.Printf("Config2: %+v", config2)
+
+	})
+
+}
