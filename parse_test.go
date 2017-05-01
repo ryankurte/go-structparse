754b04eaa72179980da646c85a279c05f8221e0c
Ryan Kurte <ryankurte@gmail.com>
2017-05-02 11:26:08 +1200

    Cleanup tests

--- parse_test.go
+++ parse_test.go
@@ -6,12 +6,7 @@
 package structparse
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
 	"testing"
-
-	"gopkg.in/yaml.v2"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -29,9 +24,6 @@ func (fem *FakeEnvMapper) ParseString(line string) string {
 }
 
 func TestParsing(t *testing.T) {
-
-	prefix := "TEST_"
-	delimiter := "$"
 
 	t.Run("Handles struct fields", func(t *testing.T) {
 		fem := FakeEnvMapper{"TEST", "REPLACED"}
@@ -75,40 +67,16 @@ func TestParsing(t *testing.T) {
 		fem := FakeEnvMapper{"TEST", "REPLACED"}
 		c := struct{ Fake map[string]map[string]string }{make(map[string]map[string]string)}
 		c.Fake["test1"] = make(map[string]string)
-		c.Fake["test1"]["test2"] = fem.Match
+		c.Fake["test2"] = make(map[string]string)
+		c.Fake["test1"]["test1"] = fem.Match
+		c.Fake["test1"]["test2"] = "boop"
+		c.Fake["test2"]["test1"] = "boop"
 
 		Strings(&fem, &c)
 
-		assert.EqualValues(t, fem.Replace, c.Fake["test1"]["test2"])
-	})
-
-	t.Run("Infills strings in structure from environment", func(t *testing.T) {
-
-		os.Setenv(fmt.Sprintf("%s%s", prefix, "NAME"), "APP_NAME")
-		os.Setenv(fmt.Sprintf("%s%s", prefix, "HOSTNAME"), "localhost")
-		os.Setenv(fmt.Sprintf("%s%s", prefix, "PORT"), "9009")
-
-		c := struct {
-			Name string
-			Host struct {
-				Name string
-				Port string
-			}
-		}{}
-
-		// Load configuration file
-		data, err := ioutil.ReadFile("./example.yml")
-		assert.Nil(t, err)
-
-		// Unmarshal from yaml
-		err = yaml.Unmarshal(data, &c)
-		assert.Nil(t, err)
-
-		Strings(NewEnvironmentMapper(delimiter, prefix), &c)
-
-		assert.EqualValues(t, "APP_NAME", c.Name)
-		assert.EqualValues(t, "localhost", c.Host.Name)
-		assert.EqualValues(t, "9009", c.Host.Port)
+		assert.EqualValues(t, fem.Replace, c.Fake["test1"]["test1"])
+		assert.EqualValues(t, "boop", c.Fake["test1"]["test2"])
+		assert.EqualValues(t, "boop", c.Fake["test2"]["test1"])
 	})
 
 }
